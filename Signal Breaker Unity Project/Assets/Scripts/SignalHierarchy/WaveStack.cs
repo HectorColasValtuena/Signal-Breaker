33c76f2b1003ab4c36aa49fa7b37509d6b859535
HectorColasValtuena <MephistoII666@gmail.com>
2020-04-24 16:50:27 +0200

    Implemented WaveStack : ISignalStack handler

---
+++ Signal Breaker Unity Project/Assets/Scripts/SignalHierarchy/WaveStack.cs
@@ -0,0 +1,18 @@
+﻿//Keeps track of the total value of a stack of waves in the same position.
+//Offers the ability to add a single value or gather the entirety of the final result
+
+//this is a simple stack: merely adds together every value incorporated.
+public class WaveStack : ISignalStack
+{
+	private int total = 0;
+
+	void ISignalStack.AddValue (int newValue)
+	{
+		total += newValue;
+	}
+
+	int ISignalStack.GetCombinedValue ()
+	{
+		return total;
+	}
+}
