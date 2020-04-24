aa51decbd2a1f60129e669a051f83b5c7742880a
HectorColasValtuena <MephistoII666@gmail.com>
2020-04-24 18:29:53 +0200

    SignalFragment constructor

--- Signal Breaker Unity Project/Assets/Scripts/SignalHierarchy/SignalFragment.cs
+++ Signal Breaker Unity Project/Assets/Scripts/SignalHierarchy/SignalFragment.cs
@@ -1,10 +1,24 @@
 ﻿//Contains a collection of ISignalContent object. They in turn can also implement ISignalContainer so as to act as intermediary storage.
 //Objects implementing only ISignalContent are considered value containers
 
+using System.Collections.Generic;
+
 //[System.Serializable]
 public class SignalFragment : ISignalContainer, ISignalContent
 {
 	private int _offset;
+	private List<ISignalContent> contents;
+
+//Constructor
+	public SignalFragment () : this (0, null);
+	public SignalFragment (int __offset) : this (__offset, null);
+	public SignalFragment (List<ISignalContent> __contents) : this (0, __contents);
+	public SignalFragment (int __offset, List<ISignalContent> __contents)
+	{
+		_offset = __offset;
+		contents = (__contents != null) ? __contents : new List<ISignalContent>();
+	}
+//ENDOF Constructor
 
 //Implementación ISignalHandler
 	ISignalStack ISignalHandler.GetValuesAt (int position, ISignalStack collectorStack, bool recursive)
