b103c00e1bab1fc95fc7fa92e79b93d38605f89e
HectorColasValtuena <MephistoII666@gmail.com>
2020-05-01 22:03:21 +0200

    Incorporated modulus operand behaviour experiments

---
+++ Signal Breaker Unity Project/Assets/Scripts/tests/ModulusSexperiments.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModulusSexperiments : MonoBehaviour
+{
+	// Start is called before the first frame update
+	void Start()
+	{
+		LogModulus(0, 6);
+		LogModulus(3, 6);
+		LogModulus(6, 6);
+		LogModulus(9, 6);
+		LogModulus(12, 6);
+		LogModulus(-3, 6);
+		LogModulus(-6, 6);
+		LogModulus(-8, 6);
+		LogModulus(-10, 6);
+		LogModulus(-12, 6);
+		LogModulus(3, -6);
+		LogModulus(6, -6);
+		LogModulus(8, -6);
+		LogModulus(10, -6);
+		LogModulus(12, -6);
+		LogModulus(-3, -6);
+		LogModulus(-6, -6);
+		LogModulus(-8, -6);
+		LogModulus(-10, -6);
+		LogModulus(-12, -6);
+	}
+
+	void LogModulus (int first, int second)
+	{
+		Debug.Log(first + " % " + second + " = " + (first%second));
+	}
+}
