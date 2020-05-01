6c26625383f36c01d898445e4c76e6caa642b280
HectorColasValtuena <MephistoII666@gmail.com>
2020-05-01 20:40:04 +0200

    Added GetValuesAt method to ISignalHandler Interface

--- Signal Breaker Unity Project/Assets/Scripts/SignalHierarchy/Interfaces/ISignalHandler.cs
+++ Signal Breaker Unity Project/Assets/Scripts/SignalHierarchy/Interfaces/ISignalHandler.cs
@@ -3,7 +3,13 @@
 //Recomendable implementar e invocar explícitamente como ISignalHandler.GetValuesAt
 public interface ISignalHandler
 {
-	//fetch values at position. Saves them into collectorStack if provided, or a new ISignalStack object.
-	//Returns a pointer to received/newly created ISignalStack object.
-	ISignalStack GetValuesAt (int position, ISignalStack collectorStack = null, bool recursive = true);
+	//Determina si este contenedor contiene algún valor en la posición objetivo. ignora los contenedores hijos si recursive = false.
+	//Si loopLength > 0, la búsqueda incluye todos los valores en posiciones múltiplo de loopLength.
+	bool HasValuesAt (int position, uint loopLength = 0, bool recursive = true);
+
+	//Busca (recursivamente?) entre todos sus hijos, acumulando un listado de todos los contenidos encontrados en la posición correspondiente.
+	//Si se le pasa un array collectionStack añade los valores a este, crea uno nuevo de lo contrario.
+	//Si loopLength > 0, la búsqueda incluye todos los valores en posiciones múltiplo de loopLength
+	//Devuelve un puntero al objeto collectorStack utilizado.
+	ISignalStack GetValuesAt (int position, ISignalStack collectorStack = null, uint loopLength = 0, bool recursive = true);
 }
