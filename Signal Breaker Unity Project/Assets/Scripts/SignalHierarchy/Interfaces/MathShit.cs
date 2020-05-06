2e1593595ccf4a098db076528a818ad4a33cc842
HectorColasValtuena <MephistoII666@gmail.com>
2020-05-06 06:48:41 +0200

    Incorporated position calculator method to temporary static class

---
+++ Signal Breaker Unity Project/Assets/Scripts/SignalHierarchy/Interfaces/MathShit.cs
@@ -0,0 +1,24 @@
+﻿public static class MathShit
+{
+	//removes offset from original position and calculates loop overflow (if loopLength >0)
+	public static int AbsoluteToRelativePosition (int position, int offset = 0, uint loopLength = 0)
+	{
+		position -= offset;
+
+		if (loopLength == 0)
+		{
+			return position;
+		}
+
+		//if a loopLength is provided calculate overflow
+		int loopedPosition = position % (int)loopLength;
+		//ensure loop-relative position is in the positive range
+		if (loopedPosition < 0) { loopedPosition += loopLength; }
+
+		return loopedPosition;
+
+		//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+		//[TO-DO] [TEST-ME]
+		//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	}
+}
