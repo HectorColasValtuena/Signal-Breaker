19253aafd3c801b65e349a9c5f0f60fb573600d4
HectorColasValtuena <MephistoII666@gmail.com>
2020-05-16 16:34:25 +0200

    Canvas-anchored viewport prototype

---
+++ Signal Breaker Unity Project/Assets/Scripts/WorldScreen/Prototypes/ViewportCanvasAnchored.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace WorldScreen 
+{
+	public class ViewportCanvasAnchored : MonoBehaviour, IWSViewport
+	{
+		RectTransform rectTransform;
+
+		void Start()
+		{
+			rectTransform = GetComponent<RectTransform>();
+		}
+
+
+	//Implementación IViewport
+		Vector2 IWSViewport.viewportSize { get { return rectTransform.rect.size; } }
+		Transform IWSViewport.rootTransform { get { return transform; } }
+
+		bool IWSViewport.IsWithin (Vector2 point)
+		{
+			return (
+				( point.x <= rectTransform.rect.size.x && point.x >= 0 ) &&
+				( point.y <= rectTransform.rect.size.y && point.y >= 0 )
+			);
+		}
+
+		//public Vector3 IViewport.ViewportToWorldPoint (Vector2 ViewportPoint) {}
+
+	//ENDOF Implementación IViewport
+	}
+}
