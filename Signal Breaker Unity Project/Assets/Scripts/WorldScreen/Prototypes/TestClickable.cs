6fa6413df3f125834d8336c6b7629102453fa7c1
HectorColasValtuena <MephistoII666@gmail.com>
2020-05-16 16:34:02 +0200

    created folder "WorldScreen/Prototypes"

---
+++ Signal Breaker Unity Project/Assets/Scripts/WorldScreen/Prototypes/TestClickable.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class TestClickable : MonoBehaviour, WorldScreen.IWSClickable
+{
+	void WorldScreen.IWSClickable.Clicked ()
+	{
+		Debug.Log("TestClickable.Clicked(); executed.");
+	}
+}
