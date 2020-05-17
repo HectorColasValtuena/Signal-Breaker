9e0c50d99cbec591df4fa9575ef13c4542a7e677
HectorColasValtuena <MephistoII666@gmail.com>
2020-05-17 21:05:34 +0200

    mousehandler changes

--- Signal Breaker Unity Project/Assets/Scripts/WorldScreen/Prototypes/MouseHandler.cs
+++ Signal Breaker Unity Project/Assets/Scripts/WorldScreen/Prototypes/MouseHandler.cs
@@ -1,18 +1,21 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MouseHandler : MonoBehaviour
+namespace WorldScreen
 {
-	// Start is called before the first frame update
-	void Start()
+	public class MouseHandler : MonoBehaviour
 	{
-		
-	}
+		// Start is called before the first frame update
+		void Start()
+		{
+			
+		}
 
-	// Update is called once per frame
-	void Update()
-	{
-		
+		// Update is called once per frame
+		void Update()
+		{
+			
+		}
 	}
 }
