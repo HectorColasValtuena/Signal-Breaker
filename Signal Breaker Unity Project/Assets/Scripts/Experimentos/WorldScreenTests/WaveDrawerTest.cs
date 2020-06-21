de3bd9e8542e50501755317e9c298cba6789ccc7
HectorColasValtuena <MephistoII666@gmail.com>
2020-06-21 19:16:34 +0200

    worldScreen WaveDrawer class and test scaffold implemented

---
+++ Signal Breaker Unity Project/Assets/Scripts/Experimentos/WorldScreenTests/WaveDrawerTest.cs
@@ -0,0 +1,15 @@
+﻿using SignalHierarchy;
+using WordlScreen;
+using UnityEngine;
+
+public class WaveDrawerTest : MonoBehaviour
+{
+	public WaveDrawer waveDrawer;
+	public int value;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		waveDrawer.setParametersAndRedraw(new Wave(value) as ISignalValue);
+	}
+}
--- Signal Breaker Unity Project/Assets/Scripts/WorldScreen/Elements/SignalElements/WaveDrawer.cs
+++ Signal Breaker Unity Project/Assets/Scripts/WorldScreen/Elements/SignalElements/WaveDrawer.cs
@@ -1,43 +1,65 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
+using SignalHierarchy;
 using UnityEngine;
 
-public class WaveDrawer : MonoBehaviour
+namespace WordlScreen
 {
-	public float intensity;
-	public float length;
-	private int pointDensity = 42;
-
-	public LineRenderer lineRenderer;
-
-	//Este método actualiza el lineRenderer según las propiedades del objeto
-	public void UpdateLine ()
+	public class WaveDrawer : MonoBehaviour
 	{
-		//lista de puntos que vamos a inyectar al lineRenderer
-		Vector3[] pointList = new Vector3[pointDensity + 2];
+		private int pointDensity = 42;
+		private ISignalValue wave;
+		private float length;
 
-		//iterar sobre la lista de puntos rellenandola
-		for (int i = 0; i <= (pointDensity+1); i++)
+		public LineRenderer lineRenderer;
+
+		//Configura la onda e inicia un re-dibujado
+		//targetWave es el objeto que contendrá el valor que queremos dibujar
+		//si length > 0, la onda medirá length de punta a punta. Si omitido o < 0, cogerá length de la anchura del contenedor
+		//si se provee material, ajustaremos el material usado por el LineRenderer
+		public void setParametersAndRedraw (ISignalValue targetWave, float targetLength = -1.0f, Material material = null)
 		{
-			//creamos un nuevo vector3 y lo introducimos en la posición correcta de la lista
-			//Vector3 (x, y, z)
-			//X será la longitud
-			//Y será el seno de la intensidad
-			float horzPos = (Mathf.PI/(pointDensity+1)) * i;
+			if (targetWave == null) { Debug.LogWarning("no ISignalValue element provided to render @WaveDrawer.RedrawLine();"); return; }
 
-			pointList[i] = new Vector3(
-				(((Mathf.Cos(horzPos) - 1) * -1)/2) * length, //Cambiamos el coseno de un rango (-1, 1) a un rango (0, 1)
-				 Mathf.Sin(horzPos) * intensity,
-				 0
-			);
+			wave = targetWave;
+					//(condición) ? true : false
+			length = (targetLength > 0.0f) ? targetLength : GetLengthFromContainer();
+			if (material != null) { lineRenderer.material = material; }
+
+			RedrawLine();
 		}
 
-		lineRenderer.positionCount = pointDensity + 2;
-		lineRenderer.SetPositions(pointList);
-	}
 
-	public void Start ()
-	{
-		UpdateLine();
+		//Este método actualiza el lineRenderer según las propiedades del objeto
+		public void RedrawLine ()
+		{
+			if (wave == null) { Debug.LogWarning("no ISignalValue element provided to render @WaveDrawer.RedrawLine();"); return; }
+			float intensity = wave.Value;
+			//lista de puntos que vamos a inyectar al lineRenderer
+			Vector3[] pointList = new Vector3[pointDensity + 2];
+
+			//iterar sobre la lista de puntos rellenandola
+			for (int i = 0; i <= (pointDensity+1); i++)
+			{
+				//creamos un nuevo vector3 y lo introducimos en la posición correcta de la lista
+				//Vector3 (x, y, z)
+				//X será la longitud
+				//Y será el seno de la intensidad
+				float horzPos = (Mathf.PI/(pointDensity+1)) * i;
+
+				pointList[i] = new Vector3(
+					(((Mathf.Cos(horzPos) - 1) * -1)/2) * length, //Cambiamos el coseno de un rango (-1, 1) a un rango (0, 1)
+					 Mathf.Sin(horzPos) * intensity,
+					 0
+				);
+			}
+
+			lineRenderer.positionCount = pointDensity + 2;
+			lineRenderer.SetPositions(pointList);
+		}
+
+		private float GetLengthFromContainer () {
+			Debug.Log((transform as RectTransform).rect.width);
+			return (transform as RectTransform).rect.width;
+		}
 	}
 }
